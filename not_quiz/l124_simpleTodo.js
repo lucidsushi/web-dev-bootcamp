c6363f8726c6520c8337c790f95f46bd0affdaf0
Sushi Girl <snowbow2@gmail.com>
2017-08-20 00:27:38 -0400

    todolist

--- not_quiz/l124_simpleTodo.js
+++ not_quiz/l124_simpleTodo.js
@@ -1,16 +1,16 @@
 function todo_list(){
-	var todo = prompt("What would you like to do?");
+	var todo = undefined;
 	var todos = [];
 
 	while(todo !== "quit"){
+		var todo = prompt("What would you like to do? (new, list or quit)");
 		if(todo === "new"){
-			var new_todo = prompt("Eneter a new todo");
+			var new_todo = prompt("Enter a new todo..");
 			todos.push(new_todo);
-		} else {
-			console.log(todos);
+		} else if(todo === "list"){
+			alert(todos);
 		}
 	}	
 }
-
 
 todo_list();
