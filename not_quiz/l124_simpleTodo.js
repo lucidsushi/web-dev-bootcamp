d54d170bf153e6af9d53ba43cfc0418d5c5cf4ec
dsu <dsu@spinvfx.com>
2017-08-21 18:51:43 -0400

    clean up todos

--- not_quiz/l124_simpleTodo.js
+++ not_quiz/l124_simpleTodo.js
@@ -1,42 +1,50 @@
-function todo_list(){
-	var option = undefined;
-	var todos = [];
+function todoList(){
+    var option = undefined;
+    var todos = [];
 
-	while(option !== "quit"){
-		var option = prompt("Enter an option: \
-							 (new, delete, list or quit)");
-		option = option.toLowerCase();
-		if(option === "new"){
-			var new_todo = prompt("Enter a new todo...");
-			todos.push(new_todo);
-			console.log(new_todo + " added to todo list.");
-		} else if(option === "list"){
-			var visual_todo = visualize_todos(todos);
-			console.log(visual_todo);
-			alert(visual_todo);
-		} else if(option === "delete"){
-			delete_todo(todos);
-		}
-	}//while loop
+    while(option !== "quit"){
+        var option = prompt("Enter an option: \
+                             (new, delete, list or quit)");
+        option = option.toLowerCase();
+        if(option === "new"){
+            newTodo(todos);
+        } else if(option === "list"){
+            listTodo(todos);
+        } else if(option === "delete"){
+            deleteTodo(todos);
+        }
+    }//while loop
 }
 
-function visualize_todos(todos){
-	//return a block string visualizing todo list
-	visual_todo = "**********\n";
-	todos.forEach(function(todo, index){
-		visual_todo += index + " : " + todo + "\n";
-	})
-	visual_todo += "**********\n";
-	return visual_todo;
+function newTodo(todos){
+    var todo = prompt("Enter a new todo...");
+    todos.push(todo);
+    console.log(todo + " added to todo list.");
 }
 
-function delete_todo(todos){
-	//ask for index of item to delete and delete it from todos
-	var visual_todo = visualize_todos(todos);
-	var index_to_delete = prompt("Enter index of item to delete:\n"
-								 + visual_todo);
-	alert(todos[index_to_delete] + " is removed.");
-	todos.splice(index_to_delete, 1);
+function listTodo(todos){
+    var visualTodo = visualizeTodos(todos);
+    console.log(visualTodo);
+    alert(visualTodo);
 }
 
-todo_list();
+function deleteTodo(todos){
+    //ask for index of item to delete and delete it from todos
+    var visualTodo = visualizeTodos(todos);
+    var index_to_delete = prompt("Enter index of item to delete:\n"
+                                 + visualTodo);
+    alert(todos[index_to_delete] + " is removed.");
+    todos.splice(index_to_delete, 1);
+}
+
+function visualizeTodos(todos){
+    //return a block string visualizing todo list
+    visualTodo = "**********\n";
+    todos.forEach(function(todo, index){
+        visualTodo += index + " : " + todo + "\n";
+    })
+    visualTodo += "**********\n";
+    return visualTodo;
+}
+
+todoList();
