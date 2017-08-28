f8363489e5c83d695dc4d2833bd2b3292a169706
Sushi Girl <snowbow2@gmail.com>
2017-08-28 00:52:03 -0400

    scorekeeper

--- not_quiz/l154_scoreKeeper.js
+++ not_quiz/l154_scoreKeeper.js
@@ -4,20 +4,23 @@
 // 	document.body.classList.toggle("toggleColor");
 // });
 
-
-playerOne = document.querySelector("#p1");
-playerTwo = document.querySelector("#p2");
+p1 = document.querySelector("#p1");
+p2 = document.querySelector("#p2");
+p1Count = document.querySelector("#p1Count");
+p2Count = document.querySelector("#p2Count");
+scoreMax = document.querySelector("#scoreMax");
 reset = document.querySelector("#reset");
-playerOne.addEventListener("click", addOneTo);
-playerTwo.addEventListener("click", addOneTo);
-reset.addEventListener("click", function(){
-	document.querySelector("#p1Count").textContent = 0;
-	document.querySelector("#p2Count").textContent = 0;
-});
 
-function addOneTo(){
-	//add 1 score to player one/two
-	counterSelector = "#" + this.id + "Count";
-	counter = document.querySelector(counterSelector);
-	counter.textContent = parseInt(counter.textContent) + 1;
+p1.addEventListener("click", addOneTo.bind(null, p1Count));
+p2.addEventListener("click", addOneTo.bind(null, p2Count));
+reset.addEventListener("click", resetCount.bind(null, [p1Count, p2Count]));
+
+function addOneTo(pCount){
+	pCount.textContent = parseInt(pCount.textContent) + 1;
+}
+
+function resetCount(pCounts){
+	pCounts.forEach(function(pCount){
+		pCount.textContent = 0;
+	});
 }
