d6c731439b87374cb5d81e343afb02365f775d53
Sushi Girl <snowbow2@gmail.com>
2017-08-28 22:55:57 -0400

    scoreKeep 2nd attempt no event removal

--- not_quiz/l154_scoreKeeper.js
+++ not_quiz/l154_scoreKeeper.js
@@ -11,42 +11,45 @@ var p2Count = document.querySelector("#p2Count");
 var scoreMax = document.querySelector("#scoreMax");
 var scoreMaxManual = document.querySelector("#scoreMaxManual");
 var reset = document.querySelector("#reset");
-//bind returns a new function object
 var pCounts = [p1Count, p2Count];
-var p1score = 0;
-var p2score = 0;
+var pScores = {p1: 0, p2: 0};
 var gameover = false;
 
-p1.addEventListener("click", manageCount.bind(null, p1Count, p1score));
-p2.addEventListener("click", manageCount.bind(null, p2Count, p2score));
+p1.addEventListener("click", manageCount.bind(null, p1Count, pScores, "p1"));
+p2.addEventListener("click", manageCount.bind(null, p2Count, pScores, "p2"));
 scoreMaxManual.addEventListener("input", alterScoreMax);
 reset.addEventListener("click", resetCount.bind(null, pCounts));
 
+function manageCount(pCount, pScores, pKey){
+	pScores[pKey] += 1;
+	pScore = pScores[pKey];
+
+	if(!gameover){
+		if(pScore >= Number(scoreMax.textContent)){
+			gameover = true;
+			pCount.classList.add("gameOverColor");
+		}
+		pCount.textContent = pScore;
+	}
+	
+}
+
 function alterScoreMax(){
-	scoreMax.textContent = this.value;
+	alteredValue = this.value;
+	if(alteredValue > 0){
+		scoreMax.textContent = alteredValue;	
+	}	
 }
 
 function resetCount(pCounts){
 	pCounts.forEach(function(pCount){
-		pCount.style.color = "black";
 		pCount.textContent = 0;
-		p1score = 0;
-		p2score = 0;
+		pScores["p1"] = 0;
+		pScores["p2"] = 0;
 	});
-}
-
-function manageCount(pCount, pScore){
-	pScore += 1;
-
-	if(pScore === parseInt(scoreMax.textContent)){
-		console.log("max is reached")
-		gameover = true;
-		pCount.style.color = "greenyellow";
-	}
-	if(!gameover){
-		console.log("not game over yet");
-		pCount.textContent = pScore;
-	}
+	p1Count.classList.remove("gameOverColor");
+	p2Count.classList.remove("gameOverColor");
+	gameover = false;
 }
 
 resetCount(pCounts);
