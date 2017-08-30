dca93f1253b8c9789262e7fc8f7538400018c624
dsu <dsu@spinvfx.com>
2017-08-30 18:28:39 -0400

    new colors/play again button

--- not_quiz/l162_colorGame.js
+++ not_quiz/l162_colorGame.js
@@ -1,20 +1,32 @@
 var rgbDisplay = document.querySelector("#rgbDisplay");
 var squares = document.querySelectorAll(".square");
-var rightAnswer = {answer: getAnswerSeed()};
+var rgbGameDisplay = document.querySelector("#rgbGameDisplay");
+var resultDisplay = document.querySelector("#resultDisplay");
+var newColorsBtn = document.querySelector("#resetColors");
+var rightAnswer = {index: null, answer: null};
+var newColorsBtnOptions = {newcolors: "New Colors", playagain: "Play Again?"}
 
-for(var i = 0; i < squares.length; i++){
-  var rgb = randomRgb();
-  if(i === rightAnswer["answer"]){
-    rightAnswer["answer"] = rgb;
+
+function initialize(){
+  newColorsBtn.value = newColorsBtnOptions.newcolors;
+  rightAnswer["index"] = getAnswerIndex();
+
+  rgbGameDisplay.style.backgroundColor = "rgb(25, 25, 25)";
+  newColorsBtn.addEventListener("click", initialize);
+
+  for(var i = 0; i < squares.length; i++){
+    var rgb = randomRgb();
+    if(i === rightAnswer["index"]){
+      rightAnswer["answer"] = rgb;
+    }
+    rgbDisplay.textContent = rightAnswer["answer"];
+    squares[i].style.backgroundColor = rgb;
+    squares[i].addEventListener("click", evalChoice);
   }
-  squares[i].style.backgroundColor = rgb;
-  squares[i].addEventListener("click", evalChoice);
 }
 
-rgbDisplay.textContent = rightAnswer["answer"];
-
-function getAnswerSeed(){
-  return Math.floor(Math.random() * 5);
+function getAnswerIndex(){
+  return Math.floor(Math.random() * 6);
 }
 
 function randomRgb(){
@@ -22,17 +34,30 @@ function randomRgb(){
   for(var i = 0; i < 3; i++){
     rgbList.push(Math.floor(Math.random() * 256));
   }
-  var rgb = "rgb(" + rgbList[0] + ", " + rgbList[1] + ", " + rgbList[2] + ")";
+  var rgb = `rgb(${rgbList[0]}, ${rgbList[1]}, ${rgbList[2]})`;
   return rgb;
 }
 
 function evalChoice(){
   if(this.style.backgroundColor === rightAnswer["answer"]){
-    console.log("correct guess");
+    resultDisplay.textContent = "You've guessed correctly!";
+    resultRightAnswer();
   } else {
-    console.log("incorrect guess");
+    resultDisplay.textContent = "Wrong guess, try again!";
+    this.style.backgroundColor = "rgb(25, 25, 25)";
   }
 }
 
+function resultRightAnswer(){
+    //button text change
+    newColorsBtn.value = newColorsBtnOptions.playagain;
+
+    //color change
+    rgbGameDisplay.style.backgroundColor = rightAnswer["answer"];
+    for(var i = 0; i < squares.length; i++){
+      squares[i].style.backgroundColor = rightAnswer["answer"];
+    }
+}
 
 
+initialize();
