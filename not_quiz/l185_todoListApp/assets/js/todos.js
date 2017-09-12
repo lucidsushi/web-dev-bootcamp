9779e61855376b54100d064b95d283fc6a01df73
dsu <dsu@spinvfx.com>
2017-09-12 18:02:31 -0400

    minimal todo pre clean up (gave up on edit feature)

--- not_quiz/l185_todoListApp/assets/js/todos.js
+++ not_quiz/l185_todoListApp/assets/js/todos.js
@@ -3,18 +3,31 @@ $("body").on("click", function(){
 	console.log('event bubbled up to body, NOOO');
 });
 
-
-//check off todos when clicked
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("checkedOff");
-});
-
-//remove to do when "X"(span) is clicked on
-$("ul").on("click", "span", removeTodo);
-
 //adding a new todo
 $("input[type='text']").on("keypress", newToDo);
 
+//check off todos when clicked
+$("ul").on("click", "li", checkedOff);
+
+//remove to do when "X"(span) is clicked on
+$("ul").on("click", ".removeLi", removeTodo);
+
+// //edit a current todo
+// $("ul").on("click", ".editLi", editTodo);
+
+function newToDo(event){
+	if(event.which === 13){
+		$("ul").append(`<li>
+							<span class="removeLi">
+								<i class="fa fa-trash" aria-hidden="true"></i>
+							</span>
+							${$(this).val()}
+						</li>`);
+		$(this).val("");
+	}
+
+	event.stopPropagation();
+}
 
 function checkedOff(event){
 	$(this).toggleClass("checkedOff");
@@ -23,14 +36,8 @@ function checkedOff(event){
 
 function removeTodo(event){
 	$(this).parent().fadeOut(function(){
-		$(this).remove();	
+		$(this).remove();
 	})
 	event.stopPropagation();
 }
 
-function newToDo(event){
-	if(event.which === 13){
-		$("ul").append(`<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ${$(this).val()}</li>`);
-		$(this).val("");
-	}
-}
